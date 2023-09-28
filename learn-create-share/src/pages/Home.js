32575d2350a3ecd94366c320bb8d8d4454c7bc6d
EthanMacleodUni <ethandavidfrancis@gmail.com>
2023-09-28 19:57:36 +1300

    Started home page

--- learn-create-share/src/pages/Home.js
+++ learn-create-share/src/pages/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import "../styles/Home.css"
+
 function Home() {
   return (
-    <div><h2>Home</h2></div>
+    <div className='home'>
+      <div className='topButtons'>
+        <button><h3>Sign up</h3></button>
+        <button><h3>Login</h3></button>
+      </div>
+      <div className='mainContent'>
+        <div className='homeTitle'><h1>Blogging for Students</h1></div>
+        <div className='branding'>right side</div>
+      </div>
+      <div className='bottomButtons'>
+
+      </div>
+      
+    </div>
   )
 }
 
