b871ed2c2b30689a0c684c0c0718abdf4b4ef92c
EthanMacleodUni <ethandavidfrancis@gmail.com>
2023-10-04 15:29:38 +1300

    Sign in limited to email domains

--- learn-create-share/src/components/NavBar.js
+++ learn-create-share/src/components/NavBar.js
@@ -4,7 +4,24 @@ import "../styles/NavBar.css"
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+      <div className='leftSide'>
+        <Link to="/" className="logoImage" style={{backgroundImage: `url(${Logo}` }}>
+        </Link>
+        <Link to="/">Learn Create Share</Link>
+      </div>
+      <div className='rightSide'>
+        <Link to="http://localhost:3000/auth/google">
+          <AccountCircleIcon/>
+        </Link>
+        <Link to="/">
+          <MoreVertIcon/>
+        </Link>
+        <Link to="/">
+          <TravelExploreIcon/>
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- server/app/routes/routes.js
+++ server/app/routes/routes.js
@@ -1,10 +1,7 @@
-const passportConfig = require("../config/passportConfig");
-
 const passport = require("passport");
 require("../config/passportConfig")(passport);
 
-module.exports = app => {
-
+module.exports = (app) => {
   // Import controllers for each table
   const users = require("../controllers/user.controller");
   const posts = require("../controllers/post.controller");
@@ -12,87 +9,85 @@ module.exports = app => {
   const comment = require("../controllers/comment.controller");
 
   // ----------------------------------------------- Users Routes ----------------------------------------------------//
-  app.route("/users")
-    .get(users.findAll)                       // The router to get all of the users
-    .post(users.create);                      // The router to create a new user
+  app
+    .route("/users")
+    .get(users.findAll) // The router to get all of the users
+    .post(users.create); // The router to create a new user
 
-  app.route("/users/userID/:userID")
-    .get(users.findByID)                      // The router to get the user by their id 
-    .delete(users.removeByID)                 // The router to remove a specific user
-    .put(users.editUserByID);                 // The router to edit user details by their id  | needs fixing
+  app
+    .route("/users/userID/:userID")
+    .get(users.findByID) // The router to get the user by their id
+    .delete(users.removeByID) // The router to remove a specific user
+    .put(users.editUserByID); // The router to edit user details by their id  | needs fixing
 
-    app.route("/users/googleID/:googleID")
-    .get(users.findByGoogleID);               // The router to get the user by their google id 
+  app.route("/users/googleID/:googleID").get(users.findByGoogleID); // The router to get the user by their google id
 
   // -----------------------------------------------------------------------------------------------------------------//
 
   // ----------------------------------------------- Post Routes -----------------------------------------------------//
-  app.route("/posts")
-    .get(posts.getAllPosts)                   // The router to get all posts
-    .post(posts.createPost);                  // The router to create a new post
+  app
+    .route("/posts")
+    .get(posts.getAllPosts) // The router to get all posts
+    .post(posts.createPost); // The router to create a new post
 
-  app.route("/posts/postID/:postID")
-    .get(posts.getPostByID)                   // The router to get posts by ID
-    .delete(posts.removePostByID)             // The router to remove posts by ID
-    .put(posts.editPostByID);                 // The router to edit posts by ID
+  app
+    .route("/posts/postID/:postID")
+    .get(posts.getPostByID) // The router to get posts by ID
+    .delete(posts.removePostByID) // The router to remove posts by ID
+    .put(posts.editPostByID); // The router to edit posts by ID
 
-  app.route("/posts/userID/:userID")
-    .get(posts.getPostsByUser);               // The router to get all posts made by a user
+  app.route("/posts/userID/:userID").get(posts.getPostsByUser); // The router to get all posts made by a user
 
-  app.route("/posts/classID/:classID")
-    .get(posts.getPostsByClass);              // The router to get all posts made by a class
+  app.route("/posts/classID/:classID").get(posts.getPostsByClass); // The router to get all posts made by a class
 
   // -----------------------------------------------------------------------------------------------------------------//
 
   // ----------------------------------------------- Class Routes ----------------------------------------------------//
-  app.route("/class")
-    .get(classes.getAllClasses)               // The router to get all classes
-    .post(classes.createClass);               // The router to create a new class
+  app
+    .route("/class")
+    .get(classes.getAllClasses) // The router to get all classes
+    .post(classes.createClass); // The router to create a new class
 
-  app.route("/class/classID/:classID")
-    .get(classes.getByID)                     // The router to get a class by ID
-    .delete(classes.removeClassByID);         // The router to remove a class by ID
+  app
+    .route("/class/classID/:classID")
+    .get(classes.getByID) // The router to get a class by ID
+    .delete(classes.removeClassByID); // The router to remove a class by ID
 
-  app.route("/class/className/:className")
-    .get(classes.getByName);                  // The router to get a class by name
-  
+  app.route("/class/className/:className").get(classes.getByName); // The router to get a class by name
+
   // -----------------------------------------------------------------------------------------------------------------//
 
   // ----------------------------------------------- Comment Routes --------------------------------------------------//
-  app.route("/comment")
-    .get(comment.getAllComments)              // The router to get all comments
-    .post(comment.createComment);             // The router to create a new comment
+  app
+    .route("/comment")
+    .get(comment.getAllComments) // The router to get all comments
+    .post(comment.createComment); // The router to create a new comment
 
-  app.route("/comment/commentID/:commentID")
-    .get(comment.getByID)                     // The router to get a comment by ID
-    .delete(comment.removeByID);              // The router to delete a comment by ID
+  app
+    .route("/comment/commentID/:commentID")
+    .get(comment.getByID) // The router to get a comment by ID
+    .delete(comment.removeByID); // The router to delete a comment by ID
 
-  app.route("/comment/postID/:postID")
-    .get(comment.getByPostID);                // The router to get comments by post
+  app.route("/comment/postID/:postID").get(comment.getByPostID); // The router to get comments by post
 
-  app.route("/comment/userID/:userID")
-    .get(comment.getByUserID);                // The router to get comments by user
-  
+  app.route("/comment/userID/:userID").get(comment.getByUserID); // The router to get comments by user
+
   // -----------------------------------------------------------------------------------------------------------------//
 
   // -------------------------------------------- Google SSO Routes --------------------------------------------------//
-  app.route("/auth/google")
-    .get(
-      passport.authenticate("google", 
-        {scope: ["email", "profile"]}
-      ));
+  app
+    .route("/auth/google")
+    .get(passport.authenticate("google", { scope: ["email", "profile"] }));
 
-  app.route("/auth/google/callback")
-    .get(
-      passport.authenticate("google", { session: false }),
-      (req, res) => {
-        res.redirect("/profile");
+  app
+    .route("/auth/google/callback")
+    .get(passport.authenticate("google", { session: false }), (req, res) => {
+      if (req.isAuthenticated()) {
+        // User is authenticated, redirect to a success page or profile page
+        res.redirect("http://localhost:3001/"); // Redirect to your desired URL
+      } else {
+        // User is not authenticated (wrong domain)
+        res.redirect("http://localhost:3000/auth/google"); // redirect to login error page (need to implement)
       }
-    );
-
-  app.route("/profile") 
-    .get((req, res) => {
-      console.log(req);
-      res.send("Welcome");
-  });
-}
+    });
+};
