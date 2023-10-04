19e7c4125aa744d73f778e30babb4cc2bb9a6318
DylanBotha-Waikato <db132@students.waikato.ac.nz>
2023-10-04 14:56:50 +1300

    Implemented RichText editor from Draft.js

---
+++ learn-create-share/src/components/DraftEditor.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Editor, EditorState } from 'draft-js';
+import '../styles/DraftEditor.css'; // Import the CSS file
+
+class DraftEditor extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+  }
+
+  onChange = (editorState) => {
+    this.setState({ editorState });
+  };
+
+  render() {
+    return (
+      <div className="draft-editor-container">
+        {/* Apply CSS class to the editor content */}
+        <div className="draft-editor-content">
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            placeholder="Start typing..."
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DraftEditor;
