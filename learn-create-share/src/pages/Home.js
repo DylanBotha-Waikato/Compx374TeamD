24b5dd8a6d6233b733da6628fd9902b4b1ce78c2
Aryan Thanki <at314@students.waikato.ac.nz>
2023-10-09 02:28:13 +1300

    Revert "Merge pull request #9 from Server"
    This reverts commit a84a4aabc87a7e65be1f1ecbe2efe769c961c6f3, reversing
    changes made to a74bb5b44630f426fbd1c7688148200629c7d08d.

--- learn-create-share/src/pages/Home.js
+++ learn-create-share/src/pages/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from 'react'
 import Logo from '../assets/combined.png'
 import {Stack, Button, ButtonGroup} from '@mui/material'
@@ -23,30 +22,6 @@ function Home() {
     
     </div>
   )
-=======
-import React from "react";
-import Logo from "../assets/combined.png";
-import { Stack, Button } from "@mui/material";
-import "../styles/Home.css";
-
-function Home() {
-  return (
-    <div>
-      <Stack direction="row" spacing={8} className="enterbuttons">
-        <Button variant="contained">Sign Up</Button>
-        <Button variant="contained">Login</Button>
-      </Stack>
-
-      <img src={Logo} className="applogo1" alt="logo" />
-
-      <h1 className="blog">Blogs for Students</h1>
-
-      <Button variant="contained" className="getstarted">
-        Get started now
-      </Button>
-    </div>
-  );
->>>>>>> Stashed changes
 }
 
-export default Home;
+export default Home
